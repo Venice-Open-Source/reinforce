9f9c5e0aa85a747eee40c11d15491b7e781123a8
nicvhub <nicolasvenegasparker@gmail.com>
2019-09-17 12:18:01 -0700

    added framework for db creation/destroy

---
+++ server/db/scripts/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users (
+  user_id serial PRIMARY KEY,
+  email VARCHAR (250) UNIQUE NOT NULL,
+  password VARCHAR (250) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cardsets (
+  cardset_id serial PRIMARY KEY,
+  username VARCHAR (250) UNIQUE NOT NULL,
+  user_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+  card_id serial PRIMARY KEY,
+  question VARCHAR (500) NOT NULL,
+  answer VARCHAR (500) NOT NULL,
+  cardset_id INTEGER REFERENCES cardsets(cardset_id)
+);
