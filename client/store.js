af1555cee33043017eb9cc174be74a07cf1ba45a
Jake Ory <jacobtory@yahoo.com>
2019-09-17 15:24:16 -0700

    fixed typo

--- client/store.js
+++ client/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import thunk from "redux-thunk";
-import reducers from "./reducers/index.sj";
+import reducers from "./reducers/index.js";
 
 const store = createStore(
   reducers,
